8de53f4d164b50c3665e33e74e561e18ebad9dc3
Bynood <quent1_jarry@hotmail.com>
2022-08-09 10:37:31 +0200

    change post seeder for 3 real posts => outing factory
    change postseeder.php for real posts

--- database/seeders/PostSeeder.php
+++ database/seeders/PostSeeder.php
@@ -14,7 +14,20 @@ class PostSeeder extends Seeder
      */
     public function run()
     {
-        \App\Models\Post::factory(1)->create();
+        // \App\Models\Post::factory(1)->create();
+        $postslist = [
+            ['description' => "Mon chat est trop mignon",
+            'img_url'=> 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Cat03.jpg/1024px-Cat03.jpg',
+            'user_id' => 1],
+            ['description' => "Ceci est un canapé",
+            'img_url'=> 'https://res.cloudinary.com/dpngcje4c/image/upload/v1657533819/canapé_nuage_qzfwq6.jpg ',
+            'user_id' => 3],
+            ['description' => "Le saphir peut prendre toutes les couleurs, sauf le rouge (dans ce cas, c'est un rubis)",
+            'img_url'=> 'https://upload.wikimedia.org/wikipedia/commons/8/83/Corundum-188152.jpg?uselang=fr',
+            'user_id' => 5 ]
+        ];
+
+        \App\Models\Post::insert($postslist);
     }
    
 }
